73169361bc83e8b48ac165cc171d8e6e345e56fb
hhn20 <harshahnarayan@gmail.com>
2020-04-23 11:58:49 +0530

    Updates

---
+++ precache-manifest.c4f6ba1e989e6bd0fa535c7e3b043239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba36063f284c038d8cef293c8010cf47",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cc760b02b1bec2f8433b",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "6225b7bb318112696f96",
+    "url": "/robofriends/static/css/main.f0ad424c.chunk.css"
+  },
+  {
+    "revision": "cc760b02b1bec2f8433b",
+    "url": "/robofriends/static/js/2.cd78ecd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cd78ecd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6225b7bb318112696f96",
+    "url": "/robofriends/static/js/main.ade64bf8.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
